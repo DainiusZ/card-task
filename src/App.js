f0a3708c8dc5dbe10032cfd63cc0e70e8869fa64
Code <you@example.com>
2022-01-24 12:56:01 +0200

    add new mapState and mapDispatch logic to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import "./App.css"
 import CardList from "./CardList";
@@ -8,62 +8,50 @@ import Spinner from "./Spinner";
 import { getApiData } from "./actions";
 
 const mapStateToProps = state =>{
-  console.log("fff")
   return {
-    data: state.cards
+    cards: state.data,
+    meta: state.meta,
+    isPending: state.isPending,
+    error: state.error
   }
 }
 
 const mapDispatchToProps = dispatch =>{
   return{
-    setCards: (data) => dispatch(getApiData(data))
+    setCards: () => dispatch(getApiData())
   }
 }
 
 const App = (props) => {
 
-  const [isLoading, setLoading] = useState(true)
-  const [cards, setCards] = useState([]);
-  const [totalItems, setTotalItems] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [cardsPerPage, setCardsPerPage] = useState(10);
+  // const [isLoading, setLoading] = useState(true)
+  // const [cards, setCards] = useState([]);
+  // const [totalItems, setTotalItems] = useState(0);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [cardsPerPage, setCardsPerPage] = useState(10);
 
+  const {cards, meta: {currentPage, cardsPerPage, totalItems}, isPending} = props;
+  
   useEffect(()=>{
-    
-    fetch("./mock_data/list.json", {
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    })
-      .then( response => response.json())
-      .then( myJson => {
-        setCards(myJson.data);
-        props.setCards(myJson.data); //playing with redux actions
-        setCurrentPage(myJson.meta.current_page);
-        setCardsPerPage(myJson.meta.per_page);
-        //čia jei meta total sutatptų su visu įrašų skaičiumi myJson.data masyve
-        // setTotalItems(myJson.meta.total); 
-        setTotalItems(myJson.data.length);
-        setLoading(false);
-      });
+    props.setCards();
   }, []);
-
+  
+  // console.log("props",props)
   const indexOfLastPost = currentPage * cardsPerPage;
   const indexOfFirstPost = indexOfLastPost - cardsPerPage;
-  const cardsToDisplay = cards.slice(indexOfFirstPost, indexOfLastPost);
+  // const cardsToDisplay = cards.slice(indexOfFirstPost, indexOfLastPost);
 
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber)
-  }
+  // const paginate = (pageNumber) => {
+  //   setCurrentPage(pageNumber)
+  // }
 
-  return isLoading ? <Spinner/> : 
+  return !isPending ? <Spinner/> : 
     (
     <div>
       <div className="cardList">
-        <CardList cards = {cardsToDisplay}/>
+        {/* <CardList cards = {cardsToDisplay}/> */}
       </div>
-        <Pagination totalItems = {totalItems} cardsPerPage = {cardsPerPage} paginate = {paginate}/>
+        {/* <Pagination totalItems = {totalItems} cardsPerPage = {cardsPerPage}/> */}
     </div>
   ) 
 }
