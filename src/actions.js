e9494e47e183cb51386a8833a3d59c3a7e90631c
Code <you@example.com>
2022-01-24 12:50:49 +0200

    add async api request to actions.js from useEffect

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,14 @@
-export const getApiData = (data) => ({
-  type: "GET_API_DATA",
-  payload: data,
-});
+export const getApiData = () => (dispatch) => {
+  dispatch({ type: "REQUEST_DATA_PENDING" });
+  fetch("./mock_data/list.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => dispatch({ type: "REQUEST_DATA_SUCCESS", payload: data }))
+    .catch((error) =>
+      dispatch({ type: "REQUEST_DATA_FAILED", payload: error })
+    );
+};
