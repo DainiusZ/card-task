75df7aec21faf70163fe3456cd3e62ac72a91344
Code <you@example.com>
2022-01-24 12:52:29 +0200

    add all action cases and states to the reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,32 @@
 const initialState = {
-  cards: []
+  cards: [],
+  meta: {},
+  isPending: false,
+  error: ""
 }
 
 export const apiData = (state = initialState, action = {}) =>{
+  // console.log(action.payload)
   switch(action.type) {
-    case "GET_API_DATA":
-      return {...state, cards: action.payload};
-    default: 
-      return state;
-  }
-}
+    case "REQUEST_DATA_PENDING":
+      return {
+        ...state, 
+        isPending: true
+      }
+      case "REQUEST_DATA_SUCCESS":
+        return  {
+          ...state, 
+          isPending: false, 
+          cards: action.payload.data, 
+          meta: action.payload.meta
+        }
+        case "REQUEST_DATA_FAILED":
+          return {      
+            ...state, 
+            isPending: false, 
+            error: action.payload
+          }
+          default: 
+          return state;
+        }
+      }
