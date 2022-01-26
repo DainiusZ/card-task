7a93be059c6a4c10079b3645745e2bd54fda5a01
Code <you@example.com>
2022-01-26 02:17:37 +0200

    remove redundant props from CardList.js

--- src/CardList.js
+++ src/CardList.js
@@ -2,16 +2,8 @@ import React from "react";
 import Card from "./Card";
 
 function CardList({ cards }) {
-  const cardsArray = cards.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        number={item.imagesCount}
-        name={item.name}
-        updated={item.updatedAt}
-        status={item.statusKey}
-      />
-    );
+  const cardsArray = cards.map((item, index) => {
+    return <Card key={item.id} id={index} data={item} />;
   });
   return cardsArray;
 }
